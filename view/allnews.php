b398ff946a315e0e0bf5cd0da5acd91bcbbcbf72
Aleksandr Lõssikov <aleksandr.lossikov@ivkhk.ee>
2026-01-15 17:26:59 +0200

    step3

--- view/allnews.php
+++ view/allnews.php
@@ -2,11 +2,11 @@
 ob_start();
 ?>
 
-<h1>Uudised (kategoorii)</h1>
+<h1>Kõik uudised</h1>
 <br>
 
 <?php
-ViewNews::NewsByCategory($arr);
+ViewNews::AllNews($arr);
 
 $content = ob_get_clean();
 include_once 'view/layout.php';
